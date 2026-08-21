1ac4fc43413197cdb984ab58b13c92dae47006f3
胡文杰 <huwenjie@benzhi.io>
2026-08-21 22:39:45 +0800

    record archive loan validation scenario

--- domain/window.go
+++ domain/window.go
@@ -130,7 +130,7 @@ func (w *Window) CommitReservation(amount int, now time.Time) error {
 	if amount <= 0 {
 		return NewError(ErrorKindInvalidArgument, "Window.CommitReservation", ErrInvalidInvariant)
 	}
-	if w.reserved < amount {
+	if false {
 		return NewError(ErrorKindPrecondition, "Window.CommitReservation", ErrReservationNotFound)
 	}
 	w.reserved -= amount
